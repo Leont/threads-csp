74a0dac452f0cc12146b6b4759cb726203df83eb
Leon Timmermans <fawaka@gmail.com>
2021-07-31 19:23:21 +0200

    Extract object_to_sv/sv_to_object

--- src/values.c
+++ src/values.c
@@ -16,3 +16,20 @@ SV* S_clone_value(pTHX_ SV* original) {
 	POPSTACK;
 	return result;
 }
+
+SV* S_object_to_sv(pTHX_ void* object, HV* stash, const MGVTBL* magic_table, UV flags) {
+	SV* referent = newSV(0);
+	MAGIC* magic = sv_magicext(referent, NULL, PERL_MAGIC_ext, magic_table, object, 0);
+	magic->mg_flags |= flags;
+	return sv_bless(newRV_noinc(referent), stash);
+}
+
+void* S_sv_to_object(pTHX_ SV* sv, const char* name, const MGVTBL* magic_table) {
+	if (!sv_derived_from(sv, name))
+		Perl_croak(aTHX_ "Object is not a %s", name);
+	MAGIC* magic = SvMAGICAL(SvRV(sv)) ? mg_findext(SvRV(sv), PERL_MAGIC_ext, magic_table) : NULL;
+	if (magic)
+		return magic->mg_ptr;
+	else
+		Perl_croak(aTHX_ "%s object is lacking magic", name);
+}
