23960cece511e9ad6938acdd678f1c2992290403
Leon Timmermans <fawaka@gmail.com>
2021-08-03 22:24:17 +0200

    Document channels and promises

---
+++ lib/threads/csp/channel.pm
@@ -0,0 +1,38 @@
+package threads::csp::channel;
+
+use strict;
+use warnings;
+
+use 5.008001;
+
+use threads::csp;
+
+1;
+
+#ABSTRACT: Channels for Communicating sequential processes
+
+__END__
+
+=head1 SYNOPSIS
+
+ my $c = threads::csp::channel->new;
+ 
+ $c->send("value");
+
+ my $rec = $c->receive;
+
+=head1 DESCRIPTION
+
+This class represents a channel between two or more CSP threads, allowing any cloneable value (unblessed values, channels and potentially others) to be passed around between threads.
+
+=method new()
+
+This creates a new channel.
+
+=method send($value)
+
+This sends a value over the channel. It will block until another thread is prepared to receive the value.
+
+=method receive()
+
+This receives a value from the channel. It will block until another thread is prepared to send the value.
